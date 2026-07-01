c190dc3d428b9a44996b905a55dfe04fdbdcd976
Teerapat <email@github.com>
2026-07-01 14:34:15 +0700

    chore: setup vite + react + typescript + tailwindcss project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<main className="flex min-h-screen items-center justify-center bg-slate-100">
+			<h1 className="text-2xl font-bold text-gray-900">Movie Survey</h1>
+		</main>
+	)
+}
+
+export default App
